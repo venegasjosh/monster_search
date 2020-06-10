e2da211859104b656fe37946b8f23e56818b7ad5
venegasjosh <venegasjosh@gmail.com>
2020-06-09 20:27:42 -0700

    Updates

---
+++ precache-manifest.2942d36a066022ef51db7547df2a0956.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42e113f5326b6bb66c6b3554d8af8d40",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "971bd0d2f633af2aad82",
+    "url": "/monsters-rolodex/static/css/main.0c45953a.chunk.css"
+  },
+  {
+    "revision": "a118fcff70fde5bdac91",
+    "url": "/monsters-rolodex/static/js/2.7bede9e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7bede9e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "971bd0d2f633af2aad82",
+    "url": "/monsters-rolodex/static/js/main.60094d59.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
